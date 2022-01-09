98cf4b8d6a32cd96feed848e529865c1c3138e3e
Wei Loong <nganweiloong@gmail.com>
2022-01-09 14:40:41 +0800

    added mediaquery for comment card

--- src/component/CommentCard.js
+++ src/component/CommentCard.js
@@ -4,29 +4,32 @@ import { deleteIcon, minus, plus, reply } from "./Icon";
 function CommentCard() {
   return (
     <div className="comment-card">
-      <header>
-        <img className="avatar" src="/images/avatars/image-amyrobson.png"></img>
-        <a href="#">amyrobson</a>
-        <span className="date-detail">1 month ago</span>
-      </header>
       <article>
+        <header>
+          <img
+            className="avatar"
+            src="/images/avatars/image-amyrobson.png"
+          ></img>
+          <a href="#">amyrobson</a>
+          <span className="date-detail">1 month ago</span>
+        </header>
         <p>
           Impressive! Though it seems the drag feature could be improved. But
           overall it looks incredible.You 've nailed the design and the
           responsiveness at various breakpoints works really well.
         </p>
-        <footer>
-          <div className="vote-number d-flex">
-            <button>{plus}</button>
-            <span>12</span>
-            <button>{minus}</button>
-          </div>
-          <button className="reply d-flex">
-            {reply}
-            <span>Reply</span>
-          </button>
-        </footer>
       </article>
+      <footer>
+        <div className="vote-number d-flex">
+          <button>{plus}</button>
+          <span>12</span>
+          <button>{minus}</button>
+        </div>
+        <button className="reply d-flex">
+          {reply}
+          <span>Reply</span>
+        </button>
+      </footer>
     </div>
   );
 }
